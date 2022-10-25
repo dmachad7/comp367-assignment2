8d864ba1a2ca7bfdccc3dd25a63efd41f4ad04c4
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2022-10-25 12:22:41 -0400

    first commit

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping("/")
+	public String greeting(Model model) {
+		model.addAttribute("greeting", "Welcome to COMP367");
+		return "index";
+	}
+	
+}
