b2d80b97f58a9753d97c445cd6b1ffb6a1cf15ba
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2022-10-27 00:35:25 -0400

    add function issue#1

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -3,14 +3,27 @@ package com.example.demo;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalTime;  
 
 @Controller
 public class AppController {
 
+
+	
 	@GetMapping("/")
 	public String greeting(Model model) {
-		model.addAttribute("greeting", "Good morning, Daniel, Welcome to COMP367");
+		getTime();
+		model.addAttribute("greeting", getTime() + ", Daniel, Welcome to COMP367");
 		return "index";
+	}
+	
+	public String getTime() {
+		int now = LocalTime.now().getHour();
+		if (now < 12) {
+			return "Good morning";
+		} else {
+			return "Good afternoon";
+		}
 	}
 	
 }
